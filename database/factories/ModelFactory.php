0eed15e157ac1a23550a2c504819cc24b9a19fe1
Eli Ottenbreit <eliotten@gmail.com>
2016-12-20 19:48:41 -0600

    Added factories for various Manic classes.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,151 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Artist::class function (Faker\Generator $faker){
+	return [
+	'name'=> $faker->word,
+	'description'=> $faker->paragraph,
+	'url'=> $faker->url,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id];
+});
+
+$factory->define(App\Language::class function (Faker\Generator $faker){
+	return [
+	'name'=> $faker->word,
+	'description'=> $faker->paragraph,
+	'url'=> $faker->url,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id];
+});
+
+$factory->define(App\Scanalator::class function (Faker\Generator $faker){
+	return [
+	'name'=> $faker->word,
+	'description'=> $faker->paragraph,
+	'url'=> $faker->url,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id];
+});
+
+$factory->define(App\Series::class function (Faker\Generator $faker){
+	return [
+	'name'=> $faker->word,
+	'description'=> $faker->paragraph,
+	'url'=> $faker->url,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id];
+});
+
+$factory->define(App\Tag::class function (Faker\Generator $faker){
+	return [
+	'name'=> $faker->word,
+	'description'=> $faker->paragraph,
+	'url'=> $faker->url,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id];
+});
+
+$factory->define(App\Rating::class function (Faker\Generator $faker){
+	return [[
+	'name' => 'Unrated', 
+	'priority'=> 0,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id],
+	['name' => 'General', 
+	'priority'=> 1,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id],],
+	['name' => 'Teen', 
+	'priority'=> 2,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id],],
+	['name' => 'Mature', 
+	'priority'=> 3,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id],],
+	['name' => 'Adult', 
+	'priority'=> 4,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id]
+	];
+)};
+
+$factory->define(App\Status::class function (Faker\Generator $faker){
+	return [
+	['name'=> 'In Progress',
+	'priority'=>0,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id],
+	['name'=> 'Completed',
+	'priority'=>1,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id],
+	['name'=> 'Cancelled',
+	'priority'=>2,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id],
+	['name'=> 'Hiatus',
+	'priority'=>3,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id]
+	];
+)};
+
+$factory->define(App\Collection::class function (Faker\Generator $faker){
+	return [
+	'name'=> $faker->sentence,
+	'description' => $faker->paragraph,
+	'canonical' => $faker->boolean,
+	'language' => App\Language::all()->random()->id,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id
+	];
+});
+
+$factory->define(App\Volume::class function (Faker\Generator $faker){
+	$collection = App\Collection::all()->random();
+	return [
+	['collection_id' => $collection->id,
+	'number' => 1,
+	'name' => $faker->sentence,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id],
+
+	['collection_id' => $collection->id,
+	'number' => 2,
+	'name' => $faker->sentence,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id],
+	
+	['collection_id' => $collection->id,
+	'number' => 3,
+	'name' => $faker->sentence,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id]];
+	});
+	
+$factory->define(App\Chapter::class function (){
+	$volume  = App\Volume::all()->random();
+	return [
+	['volume_id' => $volume->id,
+	'number' => 1,
+	'name' => $faker->sentence,
+	'source' => $faker->url,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id],
+	['volume_id' => $volume->id,
+	'number' => 2,
+	'name' => $faker->sentence,
+	'source' => $faker->url,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id]
+	['volume_id' => $volume->id,
+	'number' => 3,
+	'name' => $faker->sentence,
+	'source' => $faker->url,
+	'created_by' => App\User::all()->random()->id,
+	'updated_by'=> App\User::all()->random()->id]
+	];
+});
