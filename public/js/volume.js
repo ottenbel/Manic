20623334acd8c89ff2dd2261f8b49d96e7810a57
Eli Ottenbreit <eliotten@gmail.com>
2017-01-16 22:32:15 -0600

    Tweaked accordion code from W3School example http://www.w3schools.com/howto/howto_js_accordion.asp

--- public/js/volume.js
+++ public/js/volume.js
@@ -2,8 +2,13 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        this.classList.toggle("active");
-        this.nextElementSibling.classList.toggle("show");
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+	  if (panel.style.maxHeight){
+  	  panel.style.maxHeight = null;
+    } else {
+  	  panel.style.maxHeight = panel.scrollHeight + 'px';
+    } 
   }
 }
