4ccc9dc0d129bf5286e2543a30cd9e82dc46625f
Eli Ottenbreit <eliotten@gmail.com>
2017-01-17 21:19:17 -0600

    Add fixes to partial for creating/editing a collection so it populates correctly when a collection is passed in to it.

--- resources/views/partials/collection-input.blade.php
+++ resources/views/partials/collection-input.blade.php
@@ -48,7 +48,7 @@
 <div class="form-group">
 	{{ Form::label('language', 'Language') }}
 	@if((!empty($collection)) && ($collection->language != null) && (Input::old('language') == null))
-		{{ Form::select('language', $languages, $collection->language) }}
+		{{ Form::select('language', $languages, $collection->language->id) }}
 	@else
 		{{ Form::select('language', $languages, Input::old('language')) }}
 	@endif
@@ -56,15 +56,15 @@
 
 <div class="form-group">
 	{{ Form::label('artist_primary', 'Primary Artists') }}
-	@if((!empty($collection)) && ($collection->artist_primary != null) && (Input::old('artist_primary') == null))
-		{{ Form::text('artist_primary', implode(', ', $collection->artist_primary), array('class' => 'form-control')) }}
+	@if((!empty($collection)) && ($collection->primary_artists != null) && (Input::old('artist_primary') == null))
+		{{ Form::text('artist_primary', collect($collection->primary_artists->pluck('name'))->implode(", "), array('class' => 'form-control')) }}
 	@else
 		{{ Form::text('artist_primary', Input::old('artist_primary'), array('class' => 'form-control')) }}
 	@endif
 	
 	{{ Form::label('artist_secondary', 'Secondary Artists') }}
-	@if((!empty($collection)) && ($collection->artist_secondary != null) && (Input::old('artist_secondary') == null))
-		{{ Form::text('artist_primary', implode(', ', $collection->artist_secondary), array('class' => 'form-control')) }}
+	@if((!empty($collection)) && ($collection->secondary_artists != null) && (Input::old('artist_secondary') == null))
+		{{ Form::text('artist_primary', collect($collection->secondary_artists->pluck('name'))->implode(', '), array('class' => 'form-control')) }}
 	@else
 		{{ Form::text('artist_secondary', Input::old('artist_secondary'), array('class' => 'form-control')) }}
 	@endif
@@ -73,15 +73,15 @@
 <div class="form-group">
 	{{ Form::label('series_primary', 'Series Primary') }}
 	
-	@if((!empty($collection)) && ($collection->series_primary != null) && (Input::old('series_primary') == null))
-		{{ Form::text('series_primary', implode(', ', $collection->series_primary), array('class' => 'form-control')) }}
+	@if((!empty($collection)) && ($collection->primary_series != null) && (Input::old('series_primary') == null))
+		{{ Form::text('series_primary', collect($collection->primary_series->pluck('name'))->implode(', '), array('class' => 'form-control')) }}
 	@else
 		{{ Form::text('series_primary', Input::old('series_primary'), array('class' => 'form-control')) }}
 	@endif
 	
 	{{ Form::label('series_secondary', 'Series Secondary') }}
-	@if((!empty($collection)) && ($collection->series_secondary != null) && (Input::old('series_secondary') == null))
-		{{ Form::text('series_secondary', implode(', ', $collection->series_secondary), array('class' => 'form-control')) }}
+	@if((!empty($collection)) && ($collection->secondary_series != null) && (Input::old('series_secondary') == null))
+		{{ Form::text('series_secondary', collect($collection->secondary_series->pluck('name'))->implode(', '), array('class' => 'form-control')) }}
 	@else
 		{{ Form::text('series_secondary', Input::old('series_secondary'), array('class' => 'form-control')) }}
 	@endif
@@ -89,15 +89,15 @@
 
 <div class="form-group">
 	{{ Form::label('tag_primary', 'Tags Primary') }}
-	@if((!empty($collection)) && ($collection->tag_primary != null) && (Input::old('tag_primary') == null))
-		{{ Form::text('tag_primary', implode(', ', $collection->tag_primary), array('class' => 'form-control')) }}
+	@if((!empty($collection)) && ($collection->primary_tags != null) && (Input::old('tag_primary') == null))
+		{{ Form::text('tag_primary', collect($collection->primary_tags->pluck('name'))->implode(', '), array('class' => 'form-control')) }}
 	@else
 		{{ Form::text('tag_primary', Input::old('tag_primary'), array('class' => 'form-control')) }}
 	@endif
 	
 	{{ Form::label('tag_secondary', 'Tags Secondary') }}
-	@if((!empty($collection)) && ($collection->tag_secondary != null) && (Input::old('tag_secondary') == null))
-		{{ Form::text('tag_secondary', implode(', ', $collection->secondary), array('class' => 'form-control')) }}
+	@if((!empty($collection)) && ($collection->secondary_tags != null) && (Input::old('tag_secondary') == null))
+		{{ Form::text('tag_secondary', collect($collection->secondary_tags->pluck('name'))->implode(', '), array('class' => 'form-control')) }}
 	@else
 		{{ Form::text('tag_secondary', Input::old('tag_secondary'), array('class' => 'form-control')) }}
 	@endif
@@ -123,7 +123,7 @@
 			@endif
 	
 		@elseif((!empty($collection)) && ($collection->rating != null))
-			@if($rating->id == $collection->rating)
+			@if($rating->id == $collection->rating->id)
 				{{ Form::radio('ratings', $rating->id, true, array('id'=>"ratings-$rating->priority")) }}
 			@else
 				{{ Form::radio('ratings', $rating->id, false, array('id'=>"ratings-$rating->priority")) }}
@@ -146,7 +146,7 @@
 				{{ Form::radio('statuses', $status->id, false, array('id'=>"statuses-$status->priority")) }}
 			@endif
 		@elseif((!empty($collection)) && ($collection->status != null))
-			@if($status->id == $collection->status)
+			@if($status->id == $collection->status->id)
 				{{ Form::radio('statuses', $status->id, true, array('id'=>"statuses-$status->priority")) }}
 			@else
 				{{ Form::radio('statuses', $status->id, false, array('id'=>"statuses-$status->priority")) }}
