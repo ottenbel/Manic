62b3e81a20aabf6d5ee5ef7a46d5d7ef84110834
Eli Ottenbreit <eliotten@gmail.com>
2017-01-23 19:51:40 -0600

    The collection_id isn't passed in and can't change.  This was causing the validation to silently fail.  Should fix the problem.

--- app/Http/Controllers/VolumeController.php
+++ app/Http/Controllers/VolumeController.php
@@ -121,9 +121,8 @@ class VolumeController extends Controller
 						'integer',
 						'min:0',
 						Rule::unique('volumes')->where(function ($query){
-							$query->where('collection_id', Input::get('collection_id'));
+							$query->where('collection_id', $volume->collection->id);
 						})],
-			'collection_id' => 'required|exists:collections,id',
 			'image' => 'nullable|image'
 		]);
 		
