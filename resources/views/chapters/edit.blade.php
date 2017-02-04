d7ca1b3b19ffea3b48a030fafe813516a52502d7
Eli Ottenbreit <eliotten@gmail.com>
2017-02-04 15:58:30 -0600

    Added chapter edit page.  Added links to the chapter edit page on the volume edit page.

--- resources/views/chapters/edit.blade.php
+++ resources/views/chapters/edit.blade.php
@@ -28,12 +28,22 @@ Edit Chapter # {{$chapter->number}} {{{$chapter->name}}}
 						<div>
 							<img src="{{asset($page->name)}}" class="img-responsive img-rounded" alt="Page {{$chapter->page_number}}" height="150px" width="150px">
 						</div>
+						
 						<div>
-							{{ Form::checkbox("delete_page[{{$page->page_number}}]"), Input::old("delete_page[{{$page->page_number}}]"), array("class" => "form-control")) }}
+							{{ Form::checkbox('delete_pages[]', null, Input::old("delete_page-$page->pivot->page_number"), array('id' => "delete_page-".$page->pivot->page_number)) }}
+							{{ Form::label("delete_page-".$page->pivot->page_number, 'Delete Page') }}
 						</div>
-						<div>
-						{{ Form::text("chap_page[$page->page_number]", Input::old("chap_page[$page->page_number]"), array("class" => "form-control")) }}
-						</div>
+						
+						@if(Input::old("chap_page[$page->pivot->page_number]") == null)
+							<div>
+								{{ Form::text("chap_page[$page->pivot->page_number]", $page->pivot->page_number) }}
+							</div>
+						@else
+							<div>
+								{{ Form::text("chap_page[$page->pivot->page_number]", Input::old("chap_page[$page->pivot->page_number]")) }}
+							</div>
+						@endif
+						
 					</td>
 				@if(($loop->index % 3) == 2)
 					</tr>
--- resources/views/partials/chapter-input.blade.php
+++ resources/views/partials/chapter-input.blade.php
@@ -10,9 +10,9 @@
 <div class="form-group">
 	{{ Form::label('number', 'Number') }}
 	@if((!empty($chapter)) && ($chapter->number != null) && (Input::old('number') == null))
-		{{ Form::text('number', $chapter->number, array('class' => 'form-control')) }}
+		{{ Form::text('number', $chapter->number) }}
 	@else
-		{{ Form::text('number', Input::old('number'), array('class' => 'form-control')) }}
+		{{ Form::text('number', Input::old('number')) }}
 	@endif
 	@if($errors->has('number'))
 		<div class ="alert alert-danger" id="name_errors">{{$errors->first('number')}}</div>
@@ -22,34 +22,34 @@
 <div class="form-group">
 	{{ Form::label('name', 'Name') }}
 	@if((!empty($chapter)) && ($chapter->name != null) && (Input::old('name') == null))
-		{{ Form::text('name', $chapter->name, array('class' => 'form-control')) }}
+		{{ Form::text('name', $chapter->name) }}
 	@else
-		{{ Form::text('name', Input::old('name'), array('class' => 'form-control')) }}
+		{{ Form::text('name', Input::old('name')) }}
 	@endif
 </div>
 
 <div class="form-group">
 	{{ Form::label('scanalator_primary', 'Primary Scanalators') }}
 	@if((!empty($chapter)) && ($chapter->primary_scanalators != null) && (Input::old('scanalator_primary') == null))
-		{{ Form::text('scanalator_primary', collect($chapter->primary_scanalators->pluck('name'))->implode(", "), array('class' => 'form-control')) }}
+		{{ Form::text('scanalator_primary', collect($chapter->primary_scanalators->pluck('name'))->implode(", ")) }}
 	@else
-		{{ Form::text('scanalator_primary', Input::old('scanalator_primary'), array('class' => 'form-control')) }}
+		{{ Form::text('scanalator_primary', Input::old('scanalator_primary')) }}
 	@endif
 	
 	{{ Form::label('scanalator_secondary', 'Secondary Scanalators') }}
 	@if((!empty($chapter)) && ($chapter->secondary_scanalators != null) && (Input::old('scanalator_secondary') == null))
-		{{ Form::text('scanalator_secondary', collect($chapter->secondary_scanalators->pluck('name'))->implode(', '), array('class' => 'form-control')) }}
+		{{ Form::text('scanalator_secondary', collect($chapter->secondary_scanalators->pluck('name'))->implode(', ')) }}
 	@else
-		{{ Form::text('scanalator_secondary', Input::old('scanalator_secondary'), array('class' => 'form-control')) }}
+		{{ Form::text('scanalator_secondary', Input::old('scanalator_secondary')) }}
 	@endif
 </div>
 
 <div class="form-group">
 	{{ Form::label('source', 'Source URL') }}
 	@if((!empty($chapter)) && ($chapter->source != null) && (Input::old('source') == null))
-		{{ Form::text('source', $chapter->source, array('class' => 'form-control')) }}
+		{{ Form::text('source', $chapter->source) }}
 	@else
-		{{ Form::text('source', Input::old('source'), array('class' => 'form-control')) }}
+		{{ Form::text('source', Input::old('source')) }}
 	@endif
 	@if($errors->has('source'))
 		<div class ="alert alert-danger" id="name_errors">{{$errors->first('source')}}</div>
