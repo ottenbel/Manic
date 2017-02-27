b54c2a68f1390134d90814b1764106440c2058e1
Eli Ottenbreit <eliotten@gmail.com>
2017-02-26 19:15:20 -0600

    Migrated functionality from the series class into the new class.

--- app/Series.php
+++ app/Series.php
@@ -2,42 +2,10 @@
 
 namespace App;
 
-use App\BaseManicModel;
+use App\CollectionAssociatedTagObjectModel;
 
-class Series extends BaseManicModel
+class Series extends CollectionAssociatedTagObjectModel
 {
     //Manually set the table name as we are extending a custom model instead of the eloquent one
     protected $table = 'series';
-	
-	/*
-	 * Get the mapping from series to collections.
-	 */
-	public function collections()
-	{
-		return $this->belongsToMany('App\Collection')->withTimestamps()->withPivot('primary');
-	}
-	
-	/*
-	 * Get the number of times the series is used across the site.
-	 */
-	public function usage_count()
-	{
-		return $this->collections()->count();
-	}
-	
-	/*
-	 * Get the number of times the tag is used as a primary tag across the site.
-	 */
-	public function primary_usage_count()
-	{
-		return $this->collections()->where('primary', '=', true)->count();
-	}
-	
-	/*
-	 * Get the number of times the tag is used as a secondary tag across the site.
-	 */
-	public function secondary_usage_count()
-	{
-		return $this->collections()->where('primary', '=', false)->count();
-	}
 }
