6cd48a6e9d2f91375902e5f327b23d2336da5977
Eli Ottenbreit <eliotten@gmail.com>
2017-02-26 19:17:46 -0600

    Migrated functionality from the series class into the new class.

--- app/Tag.php
+++ app/Tag.php
@@ -2,43 +2,10 @@
 
 namespace App;
 
-use Illuminate\Support\Facades\DB;
-use App\BaseManicModel;
+use App\CollectionAssociatedTagObjectModel;
 
-class Tag extends BaseManicModel
+class Tag extends CollectionAssociatedTagObjectModel
 {
     //Manually set the table name as we are extending a custom model instead of the eloquent one
     protected $table = 'tags';
-	
-	/*
-	 * Get the collections associated with the current tag.
-	 */
-	public function collections()
-	{
-		return $this->belongsToMany('App\Collection')->withTimestamps()->withPivot('primary');
-	}
-	
-	/*
-	 * Get the number of times the tag is used across the site.
-	 */
-	public function usage_count()
-	{
-		return $this->collections()->count();
-	}
-	
-	/*
-	 * Get the number of times the tag is used as a primary tag across the site.
-	 */
-	public function primary_usage_count()
-	{
-		return $this->collections()->where('primary', '=', true)->count();
-	}
-	
-	/*
-	 * Get the number of times the tag is used as a secondary tag across the site.
-	 */
-	public function secondary_usage_count()
-	{
-		return $this->collections()->where('primary', '=', false)->count();
-	}
 }
