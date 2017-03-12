b9e4ddf04d538d617483ec1729b63f3ce946901b
Eli Ottenbreit <eliotten@gmail.com>
2017-03-12 12:42:29 -0600

    Ensure that the relevant text field exists before trying to bind the autocomplete events.

--- public/js/autocomplete/collection.js
+++ public/js/autocomplete/collection.js
@@ -1,28 +1,31 @@
 //Include autocomplete for parent collection
 $(document).ready(function(){
-	$( "#parent_id" ).autocomplete({
-      source: function( request, response ) 
-	  {
-		  $.ajax({
-				url: "/api/v1/collection/namesearch",
-				type: "POST",
-				data: {searchString: request.term },
-				success: function (data)
-				{
-					console.log(data);
-					response(data);
-				},
-				error: function()
-				{
-					console.log("Error retrieving collection list from API.");
-				}
+	if ($("#parent_id").length)
+	{
+		$( "#parent_id" ).autocomplete({
+		  source: function( request, response ) 
+		  {
+			  $.ajax({
+					url: "/api/v1/collection/namesearch",
+					type: "POST",
+					data: {searchString: request.term },
+					success: function (data)
+					{
+						console.log(data);
+						response(data);
+					},
+					error: function()
+					{
+						console.log("Error retrieving collection list from API.");
+					}
+			});
+		  },
+		  minLength: 2,
+		  focus: function() {
+			  // prevent value inserted on focus
+			  return false;
+			}
 		});
-	  },
-      minLength: 2,
-      focus: function() {
-          // prevent value inserted on focus
-          return false;
-		}
-    });
+	}
 });
 
