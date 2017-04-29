aece742f21c44eddf739641e5cc2361bf0f99328
Eli Ottenbreit <eliotten@gmail.com>
2017-04-29 15:55:48 -0600

    Replaced the body of the scanalators index page with a call to the partial that passes the relevant variables in.

--- resources/views/tagObjects/scanalators/index.blade.php
+++ resources/views/tagObjects/scanalators/index.blade.php
@@ -9,57 +9,7 @@
 @endsection
 
 @section('content')
-	<div class="container">
-		@if($scanalators->count() != 0)
-			<div>
-				<div>
-					<b>Sort By:</b>
-					@if($list_type == "usage")
-						<b><a href="{{route('index_scanalator')}}?type=usage&order={{$list_order}}">Scanalator Usage</a></b> <a href="{{route('index_scanalator')}}?type=alphabetic&order={{$list_order}}">Alphabetic</a>
-					@elseif ($list_type == "alphabetic")
-						<a href="{{route('index_scanalator')}}?type=usage&order={{$list_order}}">Scanalator Usage</a> <b><a href="{{route('index_scanalator')}}?type=alphabetic&order={{$list_order}}">Alphabetic</a></b>
-					@endif
-				</div>
-				
-				<div>
-					<b>Display Order:</b>
-					@if($list_order == "asc")
-						<b><a href="{{route('index_scanalator')}}?type={{$list_type}}&order=asc">Ascending</a></b> <a href="{{route('index_scanalator')}}?type={{$list_type}}&order=desc">Descending</a>
-					@elseif($list_order == "desc")
-						<a href="{{route('index_scanalator')}}?type={{$list_type}}&order=asc">Ascending</a> <b><a href="{{route('index_scanalator')}}?type={{$list_type}}&order=desc">Descending</a></b>
-					@endif
-				</div>
-			</div>
-			
-			@foreach($scanalators as $scanalator)
-				@if((($loop->iteration - 1) % 3) == 0)
-					<div class="row">
-				@endif
-				
-				<div class="col-xs-4">
-					<span class="primary_scanalators"><a href="{{route('show_scanalator', ['scanalator' => $scanalator])}} ">{{{$scanalator->name}}} <span class="scanalator_count">({{$scanalator->usage_count()}})</span></a></span>
-				</div>
-				
-				@if((($loop->iteration - 1) % 3) == 2)			
-					</div>
-				@endif
-			@endforeach
-		<br/>
-		<br/>
-		{{ $scanalators->links() }}
-	@else
-		@can('create', App\Models\TagObjects\Scanalator\Scanalator::class)
-			<div class="text-center">
-				No scanalators have been found in the database. Add a new scanalator <a href = "{{route('create_scanalator')}}">here.</a>
-			</div>
-		@endcan
-		@cannot('create', App\Models\TagObjects\Scanalator\Scanalator::class)
-			<div class="text-center">
-				No scanalators have been found in the database.
-			</div>
-		@endcan
-	@endif
-	</div>
+	@include('partials.tagObjects.index-content', ['tagObjects' => $scanalators, 'tagObjectName' => 'scanalator', 'titleTagObjectName' => 'Scanalator', 'associatedType' => 'chapter', 'indexRoute' => 'index_scanalator', 'showRoute' => 'show_scanalator', 'createRoute' => 'create_scanalator', 'tagDisplayClass' => "primary_scanalators", 'tagDisplayCountClass' => "scanalator_count", 'classModelPath' => App\Models\TagObjects\Scanalator\Scanalator::class])
 @endsection
 
 @section('footer')
