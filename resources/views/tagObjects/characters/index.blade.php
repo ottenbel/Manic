b7669461cbf3b82eddca37eaa7632b291a561a66
Eli Ottenbreit <eliotten@gmail.com>
2017-04-29 14:46:06 -0600

    Replaced the body of the characters index page with a call to the partial that passes the relevant variables in.

--- resources/views/tagObjects/characters/index.blade.php
+++ resources/views/tagObjects/characters/index.blade.php
@@ -9,58 +9,7 @@
 @endsection
 
 @section('content')
-	<div class="container">
-		@if($characters->count() != 0)
-			<div>
-				<div>
-					<b>Sort By:</b>
-					@if($list_type == "usage")
-						<b><a href="{{route('index_character')}}?type=usage&order={{$list_order}}">Character Usage</a></b> <a href="{{route('index_character')}}?type=alphabetic&order={{$list_order}}">Alphabetic</a>
-					@elseif ($list_type == "alphabetic")
-						<a href="{{route('index_character')}}?type=usage&order={{$list_order}}">Character Usage</a> <b><a href="{{route('index_character')}}?type=alphabetic&order={{$list_order}}">Alphabetic</a></b>
-					@endif
-				</div>
-				
-				<div>
-					<b>Display Order:</b>
-					@if($list_order == "asc")
-						<b><a href="{{route('index_character')}}?type={{$list_type}}&order=asc">Ascending</a></b> <a href="{{route('index_character')}}?type={{$list_type}}&order=desc">Descending</a>
-					@elseif($list_order == "desc")
-						<a href="{{route('index_character')}}?type={{$list_type}}&order=asc">Ascending</a> <b><a href="{{route('index_character')}}?type={{$list_type}}&order=desc">Descending</a></b>
-					@endif
-				</div>
-			</div>
-
-		@foreach($characters as $character)
-			@if((($loop->iteration - 1) % 3) == 0)
-				<div class="row">
-			@endif
-			
-			<div class="col-xs-4">
-				<span class="primary_characters"><a href="{{route('show_character', ['character' => $character])}}">{{{$character->name}}} <span class="character_count">({{$character->usage_count()}})</span></a></span>
-			</div>
-			
-			@if((($loop->iteration - 1) % 3) == 2)			
-				</div>
-			@endif
-		@endforeach
-		<br/>
-		<br/>
-		{{ $characters->links() }}
-	@else
-		@can('create', App\Models\TagObjects\Character\Character::class)
-			<div class="text-center">
-				No characters have been found in the database. Add a new character <a href = "{{route('create_character')}}">here.</a>
-			</div>
-		@endcan
-		
-		@cannot('create', App\Models\TagObjects\Character\Character::class)
-			<div class="text-center">
-				No characters have been found in the database.
-			</div>
-		@endcan
-	@endif
-</div>
+	@include('partials.tagObjects.index-content', ['tagObjects' => $characters, 'tagObjectName' => 'character', 'titleTagObjectName' => 'Character', 'associatedType' => 'collection', 'indexRoute' => 'index_character', 'showRoute' => 'show_character', 'createRoute' => 'create_character', 'tagDisplayClass' => "primary_characters", 'tagDisplayCountClass' => "character_count", 'classModelPath' => App\Models\TagObjects\Character\Character::class])
 @endsection
 
 @section('footer')
