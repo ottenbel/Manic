cf7451524f636adb52985e4a68963f4203b724c4
Eli Ottenbreit <eliotten@gmail.com>
2017-04-29 16:19:51 -0600

    Replaced the body of the series index page with a call to the partial that passes the relevant variables in.
    Replaced the body of the tags index page with a call to the partial that passes the relevant variables in.

--- resources/views/tagObjects/series/index.blade.php
+++ resources/views/tagObjects/series/index.blade.php
@@ -9,58 +9,7 @@
 @endsection
 
 @section('content')
-	<div class="container">
-		@if($series->count() != 0)
-			<div>
-				<div>
-					<b>Sort By:</b>
-					@if($list_type == "usage")
-						<b><a href="{{route('index_series')}}?type=usage&order={{$list_order}}">Series Usage</a></b> <a href="{{route('index_series')}}?type=alphabetic&order={{$list_order}}">Alphabetic</a>
-					@elseif ($list_type == "alphabetic")
-						<a href="{{route('index_series')}}?type=usage&order={{$list_order}}">Series Usage</a> <b><a href="{{route('index_series')}}?type=alphabetic&order={{$list_order}}">Alphabetic</a></b>
-					@endif
-				</div>
-				
-				<div>
-					<b>Display Order:</b>
-					@if($list_order == "asc")
-						<b><a href="{{route('index_series')}}?type={{$list_type}}&order=asc">Ascending</a></b> <a href="{{route('index_series')}}?type={{$list_type}}&order=desc">Descending</a>
-					@elseif($list_order == "desc")
-						<a href="{{route('index_series')}}?type={{$list_type}}&order=asc">Ascending</a> <b><a href="{{route('index_series')}}?type={{$list_type}}&order=desc">Descending</a></b>
-					@endif
-				</div>
-			</div>
-			
-			@foreach($series as $ser)
-				@if((($loop->iteration - 1) % 3) == 0)
-					<div class="row">
-				@endif
-				
-				<div class="col-xs-4">
-					<span class="primary_series"><a href="{{route('show_series', ['series' => $ser] )}}">{{{$ser->name}}} <span class="series_count">({{$ser->usage_count()}})</span></a></span>
-				</div>
-				
-				@if((($loop->iteration - 1) % 3) == 2)			
-					</div>
-				@endif
-			@endforeach
-		<br/>
-		<br/>
-		{{ $series->links() }}
-	@else
-		@can('create', App\Models\TagObjects\Series\Series::class)
-			<div class="text-center">
-				No series have been found in the database. Add a new series <a href = "{{route('create_series')}}">here.</a>
-			</div>
-		@endcan
-		
-		@cannot('create', App\Models\TagObjects\Series\Series::class)
-			<div class="text-center">
-				No series have been found in the database.
-			</div>
-		@endcan
-	@endif
-	</div>
+	@include('partials.tagObjects.index-content', ['tagObjects' => $series, 'tagObjectName' => 'series', 'titleTagObjectName' => 'Series', 'associatedType' => 'collection', 'indexRoute' => 'index_series', 'showRoute' => 'show_series', 'createRoute' => 'create_series', 'tagDisplayClass' => "primary_series", 'tagDisplayCountClass' => "series_count", 'classModelPath' => App\Models\TagObjects\Series\Series::class])
 @endsection
 
 @section('footer')
--- resources/views/tagObjects/tags/index.blade.php
+++ resources/views/tagObjects/tags/index.blade.php
@@ -9,58 +9,7 @@
 @endsection
 
 @section('content')
-	<div class="container">
-		@if($tags->count() != 0)
-			<div>
-				<div>
-					<b>Sort By:</b>
-					@if($list_type == "usage")
-						<b><a href="{{route('index_tag')}}?type=usage&order={{$list_order}}">Tag Usage</a></b> <a href="{{route('index_tag')}}?type=alphabetic&order={{$list_order}}">Alphabetic</a>
-					@elseif ($list_type == "alphabetic")
-						<a href="{{route('index_tag')}}?type=usage&order={{$list_order}}">Tag Usage</a> <b><a href="{{route('index_tag')}}?type=alphabetic&order={{$list_order}}">Alphabetic</a></b>
-					@endif
-				</div>
-				
-				<div>
-					<b>Display Order:</b>
-					@if($list_order == "asc")
-						<b><a href="{{route('index_tag')}}?type={{$list_type}}&order=asc">Ascending</a></b> <a href="{{route('index_tag')}}?type={{$list_type}}&order=desc">Descending</a>
-					@elseif($list_order == "desc")
-						<a href="{{route('index_tag')}}?type={{$list_type}}&order=asc">Ascending</a> <b><a href="{{route('index_tag')}}?type={{$list_type}}&order=desc">Descending</a></b>
-					@endif
-				</div>
-			</div>
-			
-			@foreach($tags as $tag)
-				@if((($loop->iteration - 1) % 3) == 0)
-					<div class="row">
-				@endif
-				
-				<div class="col-xs-4">
-					<span class="primary_tags"><a href="{{route('show_tag', ['tag' => $tag])}}">{{{$tag->name}}} <span class="tag_count">({{$tag->usage_count()}})</span></a></span>
-				</div>
-				
-				@if((($loop->iteration - 1) % 3) == 2)			
-					</div>
-				@endif
-			@endforeach
-			<br/>
-			<br/>
-			{{ $tags->links() }}
-		@else
-			@can('create', App\Models\TagObjects\Tag\Tag::class)
-				<div class="text-center">
-					No tags have been found in the database. Add a new tag <a href = "{{route('create_tag')}}">here.</a>
-				</div>
-			@endcan
-			
-			@cannot('create', App\Models\TagObjects\Tag\Tag::class)
-				<div class="text-center">
-					No tags have been found in the database.
-				</div>
-			@endcan
-		@endif
-	</div>
+	@include('partials.tagObjects.index-content', ['tagObjects' => $tags, 'tagObjectName' => 'tag', 'titleTagObjectName' => 'Tag', 'associatedType' => 'collection', 'indexRoute' => 'index_tag', 'showRoute' => 'show_tag', 'createRoute' => 'create_tag', 'tagDisplayClass' => "primary_tags", 'tagDisplayCountClass' => "tag_count", 'classModelPath' => App\Models\TagObjects\Tag\Tag::class])
 @endsection
 
 @section('footer')
