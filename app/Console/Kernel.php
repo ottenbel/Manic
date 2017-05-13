2715736fc499913b613539170f744d6161a28287
Eli Ottenbreit <eliotten@gmail.com>
2017-05-12 23:45:07 -0600

    Issue #6.  Added scheduled task to the to update the file system/images table by deleting any images that aren't associated with anything (collection/volume cover or page image).

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use Storage;
+use App\Models\Image;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -26,6 +28,30 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+		
+		/*
+		 * Clean up the filesystem (delete any images that aren't linked to anything)
+		 */
+		$schedule->call(function(){
+			//Retrieve all images
+			$images = Image::all();
+			
+			foreach ($images as $image)
+			{
+				if (($image->collections()->count() == 0) && ($image->volumes()->count() == 0)
+					&& ($image->chapters()->count() == 0))
+					{
+						//Delete fullsized image
+						Storage::delete($image->name);
+						
+						//Delete thumbnail
+						Storage::delete($image->thumbnail);
+						
+						//Hard delete image
+						$image->forceDelete();
+					}
+			}
+		})->weekly();
     }
 
     /**
