0f7ba32e7fb732e093a60146b2550dabf6047bbf
Eli Ottenbreit <eliotten@gmail.com>
2017-11-16 12:58:47 -0600

    Add rating restrictions to the dropdown.

--- resources/views/partials/navbar/left.blade.php
+++ resources/views/partials/navbar/left.blade.php
@@ -10,10 +10,14 @@
 			@if(Auth::user()->has_editor_permission())
 				<li><a href="{{route('user_dashboard_configuration_placeholders')}}">Placeholders</a></li>
 			@endif
+			
+			<li><a href="{{route('user_dashboard_configuration_rating_restriction')}}">Rating Restrictions</a></li>
+			
 			@if(Auth::user()->has_administrator_permission())
 				<h6 class="dropdown-header">Administrator</h6>
 				<li><a href="{{route('admin_dashboard_configuration_pagination')}}">Pagination</a></li>
 				<li><a href="{{route('admin_dashboard_configuration_placeholders')}}">Placeholders</a></li>
+				<li><a href="{{route('admin_update_configuration_rating_restriction')}}">Rating Restrictions</a></li>
 			@endif
 		</ul>
 	</li>
