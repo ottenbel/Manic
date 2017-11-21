11a468bc1f8f3d6cac121e6a94220fecb1d82b53
Eli Ottenbreit <eliotten@gmail.com>
2017-11-20 20:35:18 -0600

    Enclosed all database changes for the pagination controller inside of transaction statements.

--- app/Http/Controllers/Configuration/PaginationController.php
+++ app/Http/Controllers/Configuration/PaginationController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\WebController;
 use Route;
 use Auth;
 use Input;
+use DB;
 use ConfigurationLookupHelper;
 use App\Models\Configuration\ConfigurationPagination;
 
@@ -66,22 +67,40 @@ class PaginationController extends WebController
 			$paginationValues = ConfigurationPagination::where('user_id', '=', null)->orderBy('priority')->get();
 		}
 		
-		//Update all values
-		//foreach ($paginationValues as $paginationValue)
-		for ($i = 0; $i < $paginationValues->count(); $i++)
+		DB::beginTransaction();
+		try
 		{
-			$paginationValue = $paginationValues[$i];
-			
-			$value = Input::get("pagination_values.".$paginationValue->key);
-			$helper = Input::get("pagination_values_helpers.".$paginationValue->key);
-			
-			if (($paginationValue->value != $value) || ($paginationValue->description != $helper))
+			//Update all values
+			for ($i = 0; $i < $paginationValues->count(); $i++)
 			{
-				$paginationValue->value = $value;
-				$paginationValue->description = $helper;
-				$paginationValue->save();
+				$paginationValue = $paginationValues[$i];
+				
+				$value = Input::get("pagination_values.".$paginationValue->key);
+				$helper = Input::get("pagination_values_helpers.".$paginationValue->key);
+				
+				if (($paginationValue->value != $value) || ($paginationValue->description != $helper))
+				{
+					$paginationValue->value = $value;
+					$paginationValue->description = $helper;
+					$paginationValue->save();
+				}
 			}
 		}
+		catch (\Exception $e)
+		{
+			DB::rollBack();
+			$messages = self::BuildFlashedMessagesVariable(null, null, ["Unable to successfully update pagination configuration changes to the database."]);
+			if (Route::is('user_update_configuration_pagination'))
+			{
+				return redirect()->route('user_dashboard_configuration_pagination')->with(["messages" => $messages])->withInput();
+			}
+			else if (Route::is('admin_update_configuration_pagination'))
+			{
+				return redirect()->route('admin_dashboard_configuration_pagination')->with(["messages" => $messages])->withInput();
+			}
+		}
+		
+		DB::commit();
 		
 		if (Route::is('user_update_configuration_pagination'))
 		{
@@ -105,22 +124,34 @@ class PaginationController extends WebController
     { 
 		$this->authorize(ConfigurationPagination::class); 
 		
-		$userPaginationValues = Auth::user()->pagination_configuration()->orderBy('priority')->get();
-		$sitePaginationValues = ConfigurationPagination::where('user_id', '=', null)->orderBy('priority')->get();
-		
-		for ($i = 0; $i < $userPaginationValues->count(); $i++)
+		DB::beginTransaction();
+		try
 		{
-			$userPagination = $userPaginationValues[$i];
-			$globalPagination = $sitePaginationValues->where('key', $userPagination->key)->first();
-			 
-			 if (($userPagination->value != $globalPagination->value) 
-				 || ($userPagination->description != $globalPagination->description))
-			 {
-				$userPagination->value = $globalPagination->value;
-				$userPagination->description = $globalPagination->description;
-				$userPagination->save();
-			 }
+			$userPaginationValues = Auth::user()->pagination_configuration()->orderBy('priority')->get();
+			$sitePaginationValues = ConfigurationPagination::where('user_id', '=', null)->orderBy('priority')->get();
+		
+			for ($i = 0; $i < $userPaginationValues->count(); $i++)
+			{
+				$userPagination = $userPaginationValues[$i];
+				$globalPagination = $sitePaginationValues->where('key', $userPagination->key)->first();
+				 
+				 if (($userPagination->value != $globalPagination->value) 
+					 || ($userPagination->description != $globalPagination->description))
+				 {
+					$userPagination->value = $globalPagination->value;
+					$userPagination->description = $globalPagination->description;
+					$userPagination->save();
+				 }
+			}
 		}
+		catch (\Exception $e)
+		{
+			DB::rollBack();
+			$messages = self::BuildFlashedMessagesVariable(null, null, ["Unable to successfully reset pagination configuration settings based on site configuration."]);
+			return redirect()->route('user_dashboard_configuration_pagination')->with(["messages" => $messages])->withInput();
+		}
+		
+		DB::commit();
 		
 		$messages = self::BuildFlashedMessagesVariable(["Successfully reset pagination configuration settings for user to site defaults."], null, null);
 		return redirect()->route('user_dashboard_configuration_pagination')->with("messages", $messages);
