540090efb26e7f5f3628181378995ee6e28435f0
Eli Ottenbreit <eliotten@gmail.com>
2018-01-14 15:59:17 -0600

    Issue #25.  Added basic functionality for the an administrator looking at an user.

--- resources/views/partials/navbar/components/right/user/admin/user.blade.php
+++ resources/views/partials/navbar/components/right/user/admin/user.blade.php
@@ -5,7 +5,7 @@
 		</a>
 		<ul class="dropdown-menu" role="menu">
 			<h6 class="dropdown-header">Indexes</h6>
-			<li><a href="{{ route('admin_index_user') }}"><i class="fa fa-unlock" aria-hidden="true"></i> Users</a></li>
+			<li><a href="{{ route('admin_index_user') }}"><i class="fa fa-user" aria-hidden="true"></i> Users</a></li>
 		</ul>
 	</li>
 @endif
--- resources/views/partials/user/admin/user/rolesAndPermissions/show-content.blade.php
+++ resources/views/partials/user/admin/user/rolesAndPermissions/show-content.blade.php
@@ -4,18 +4,27 @@
 
 <div id="rolesAndPermissions_panel" class="volume_panel">
 	<h2>Roles</h2>
-	@if($roles->count() > 0)
-		@include('partials.rolesAndPermissions.roles.role-index', ['roles' => $roles])
-	@else
-		<div>No roles are associated with user {{$user->name}}</div>
-	@endif
+	<div class ="row">
+		@if($roles->count() > 0)
+			@include('partials.rolesAndPermissions.roles.role-index', ['roles' => $roles])
+		@else
+			<div>No roles are associated with user {{$user->name}}</div>
+		@endif
+	</div>
 	
 	<h2>Permissions</h2>
-	@if($permissions->count() > 0)
-		@include('partials.rolesAndPermissions.permissions.permission-index', ['permissions' => $permissions])
-	@else
-		<div>No permissions are associated with user {{$user->name}}</div>
-	@endif
+	<div class ="row">
+		@if($permissions->count() > 0)
+			@include('partials.rolesAndPermissions.permissions.permission-index', ['permissions' => $permissions])
+		@else
+			<div>No direct permissions are associated with user {{$user->name}}. Check associated roles for inherited permissions.</div>
+		@endif
+	</div>
 	
-	<a class="btn btn-success btn-sm" href="{{route('admin_edit_user_roles_and_permissions', $user)}}"><i class="fa fa-pencil" aria-hidden="true"></i> Edit Roles and Permissions</a>
+	@can('Edit User Roles and Permissions')
+	<div class="row">
+		<a class="btn btn-success btn-sm" href="{{route('admin_edit_user_roles_and_permissions', $user)}}"><i class="fa fa-pencil" aria-hidden="true"></i> Edit Roles and Permissions</a>
+	</div>
+		
+	@endcan
 </div>
