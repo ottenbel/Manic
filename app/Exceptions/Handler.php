8fa04b05be739cd70563eb25761f8e0a17fdcae6
Eli Ottenbreit <eliotten@gmail.com>
2018-04-12 23:15:30 -0600

    Added logging functionality in top level exception handling so that any uncaught exceptions are caught and logged.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Log;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
@@ -35,6 +36,8 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
+        Log::critical('Uncaught exception encountered.', ['error' => $exception]);
+
         parent::report($exception);
     }
 
